30730d1b5f5fa670f70ca1f29fcf192cced8a50b
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2024-09-05 10:47:00 -0300

    chore: update file sumNumbers.js

--- Dia 02/sumNumbers.js
+++ Dia 02/sumNumbers.js
@@ -1,6 +1,6 @@
 let prompt = require('prompt-sync')();
-const number1 = Number(prompt("Digite o primeiro numero: "))
-const number2 = Number(prompt("Digite o segundo numero: "))
+const number1 = Number(prompt("Digite o primeiro numero: "));
+const number2 = Number(prompt("Digite o segundo numero: "));
 const sumNumbers = (number1, number2) => {
   let sum = 0;
   sum = number1 + number2;
