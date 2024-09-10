bba9099d68806a7e38bbf13ab98ef53ab98c1daf
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2024-09-10 08:44:31 -0300

    chore: Update Day 7 challenge - Multiplication Table improvements

--- Dia 07/multiplicationTable.py
+++ Dia 07/multiplicationTable.py
@@ -3,7 +3,6 @@ number = float(input("Digite um número: "))
 def multiplicationTable(number):
   print(f"Tabuada de {number}:")
   for n in range(1, 11):
-    result = number * n
     print(f"{n} * {number} = {n*number}")
 while True:
     try:
@@ -11,5 +10,5 @@ while True:
         break
     except ValueError:
         print("Por favor, digite um número válido.")
-        
+
 multiplicationTable(number)
